c8b0070ca9811fb2afc3514f62dc6cc8dfdf086a
Yura <yura5742248@gmail.com>
2024-01-09 21:57:10 +0300

    add PUT video router

--- src/routes/video-router.ts
+++ src/routes/video-router.ts
@@ -4,7 +4,7 @@ import express, {Request, Response} from 'express';
 export const videoRouter = express.Router({})
 
 
-const AvailableResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160']
+const AvailableResolutionsType = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160']
 
 // enum Quality {
 //     P144 = 'P144',
@@ -25,7 +25,7 @@ type VideoType = {
     minAgeRestriction: number | null,
     createdAt: string,
     publicationDate: string,
-    availableResolutions: typeof AvailableResolutions
+    availableResolutions: typeof AvailableResolutionsType
 }
 
 
@@ -55,11 +55,14 @@ videoRouter.get('/:id', (req: Request, res: Response) => {
         res.sendStatus(404).send('video not found')
     }
 })
-type RequestWithBody<B> = Request<unknown, unknown, B, unknown>
+type RequestWithBody<P, B> = Request<P, unknown, B, unknown>
 type CreateVideoType = {
     title: string
     author: string
-    availableResolutions: typeof AvailableResolutions
+    availableResolutions: typeof AvailableResolutionsType
+}
+type ParamIdType = {
+    id: string
 }
 type ErrorMessageType = {
     field: string
@@ -68,7 +71,7 @@ type ErrorMessageType = {
 type ErrorType = {
     errorsMessages: ErrorMessageType[]
 }
-videoRouter.post('/', (req: RequestWithBody<CreateVideoType>, res: Response) => {
+videoRouter.post('/', (req: RequestWithBody<ParamIdType, CreateVideoType>, res: Response) => {
     let errors: ErrorType = {
         errorsMessages: []
     }
@@ -81,7 +84,7 @@ videoRouter.post('/', (req: RequestWithBody<CreateVideoType>, res: Response) => 
     }
     if (Array.isArray(availableResolutions)) {
         availableResolutions.forEach(r => {
-            if (!AvailableResolutions.includes(r)) {
+            if (!AvailableResolutionsType.includes(r)) {
                 errors.errorsMessages.push({message: 'Available resolutions incorrect', field: 'availableResolutions'})
                 return
             }
@@ -126,5 +129,56 @@ videoRouter.delete('/:id', (req: Request, res: Response) => {
     res.send(404)
     return;
 
+})
+
+type VideoUpdateType = {
+    title: string
+    author: string
+    availableResolutions: typeof AvailableResolutionsType
+    canBeDownloaded: boolean
+    minAgeRestriction: any
+    publicationDate: string
+}
+videoRouter.put('/:id', (req: RequestWithBody<ParamIdType, VideoUpdateType>, res: Response) => {
+    const {title, author, availableResolutions, publicationDate, minAgeRestriction, canBeDownloaded} = req.body
+    const fieldError: ErrorType = {
+        errorsMessages: []
+    }
+    if (!title || !title.trim() || title.length > 40) {
+        fieldError.errorsMessages.push({field: 'title', message: 'Incorrect title'})
+    }
+
+    if (!author || !author.trim() || author.length > 20) {
+        fieldError.errorsMessages.push({field: 'title', message: 'Incorrect author'})
+    }
+
+    if (fieldError.errorsMessages.length > 0) {
+        res.sendStatus(400).send(fieldError.errorsMessages)
+    }
+
+    const video = videos.find(v => v.id === +req.params.id)
+
+    if (!video) {
+        res.send(404)
+        return;
+    }
+
+    let newVideo: VideoType = {
+        id: video.id,
+        author: author,
+        title: title,
+        availableResolutions: availableResolutions ? availableResolutions : video.availableResolutions,
+        createdAt: video.createdAt,
+        canBeDownloaded: canBeDownloaded ? canBeDownloaded : video.canBeDownloaded,
+        minAgeRestriction: minAgeRestriction ? minAgeRestriction : video.minAgeRestriction,
+        publicationDate: publicationDate ? publicationDate : video.publicationDate
+    }
+    const index = videos.findIndex(v => v.id === video.id)
+
+    videos.splice(index, 1, newVideo)
+
+
+    res.send(204)
+    return
 })
 
