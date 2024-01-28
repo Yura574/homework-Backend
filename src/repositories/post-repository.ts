9275dd8572fdc2009f8bcf8867a7da7df8376e4e
Yura <yura5742248@gmail.com>
2024-01-28 14:43:18 +0300

    crud posts mongodb

--- src/repositories/post-repository.ts
+++ src/repositories/post-repository.ts
@@ -1,21 +1,29 @@
-import {db} from '../db/db';
-import {BlogViewModelType, PostInputModelType, PostViewModelType} from '../models/blogModels';
-import {BlogRepository} from './blog-repository';
+import { PostInputModelType, PostViewModelType} from '../models/blogModels';
 import {blogCollection, postCollection} from '../index';
 import {ObjectId} from 'mongodb';
 
 
 export class PostRepository {
-    static getAllPost() {
-        return db.posts
+    static async getAllPost() {
+        return await postCollection.find({}).toArray()
     }
 
-    static getPostById(id: string) {
-        return postCollection.findOne({_id: new ObjectId(id)})
+    static async getPostById(id: string) {
+        const post = await postCollection.findOne({_id: new ObjectId(id)})
+        return {
+            id: post?._id,
+            blogId: post?.blogId,
+            title: post?.title,
+            shortDescription: post?.shortDescription,
+            content: post?.content,
+            createdAt: post?.createdAt,
+            blogName: post?.blogName
+        }
     }
+
     //
     static async createPost(data: PostInputModelType) {
-        const blog =await blogCollection.findOne({_id: new ObjectId(data.blogId)})
+        const blog = await blogCollection.findOne({_id: new ObjectId(data.blogId)})
         if (!blog) {
             return false
         }
@@ -34,27 +42,24 @@ export class PostRepository {
     }
 
     static async deletePost(id: string) {
-        const index =await postCollection.deleteOne({_id: new ObjectId(id)})
+        const index = await postCollection.deleteOne({_id: new ObjectId(id)})
         console.log(index)
         return true
     }
-    //
-    // static updatePost(id: string, data: PostInputModelType) {
-    //     const {title, shortDescription, content} = data
-    //     const post = db.posts.find(p => p.id === id)
-    //     if (post) {
-    //         const updatedPost: PostViewModelType = {
-    //             id,
-    //             shortDescription,
-    //             content,
-    //             title,
-    //             blogName: post.blogName,
-    //             blogId: post.blogId
-    //         }
-    //         const index = db.posts.findIndex(p => p.id === id)
-    //         db.posts.splice(index, 1, updatedPost)
-    //         return true
-    //     }
-    //     return false
-    // }
+
+
+    static async updatePost(id: string, data: PostInputModelType) {
+        const {title, shortDescription, content} = data
+        const post = await postCollection.findOne({_id: new ObjectId(id)})
+        if (post) {
+            const updatedPost = {
+                shortDescription,
+                content,
+                title,
+            }
+            await postCollection.updateOne({_id: new ObjectId(id)}, updatedPost)
+            return true
+        }
+        return false
+    }
 }
--- src/routes/post-router.ts
+++ src/routes/post-router.ts
@@ -1,24 +1,33 @@
 import express, {Request, Response} from 'express';
 import {PostRepository} from '../repositories/post-repository';
 import {HTTP_STATUSES} from '../utils/httpStatuses';
-import {db} from '../db/db';
 import {authMiddleware} from '../middleware/auth/auth-middleware';
 import {findPost, postValidation} from '../validators/post-validators';
 import {validationResult} from 'express-validator';
 import {ValidateError} from '../utils/validateError';
 import {postCollection} from '../index';
-import {PostViewModelType} from '../models/blogModels';
 
 
 export const postRouter = express.Router()
 
 postRouter.get('/', async (req: Request, res: Response) => {
-    const allPosts = PostRepository.getAllPost()
+    const allPosts = await PostRepository.getAllPost()
+    const returnAllPosts = allPosts.map(post => {
+        return {
+            id: post?._id,
+            blogId: post?.blogId,
+            title: post?.title,
+            shortDescription: post?.shortDescription,
+            content: post?.content,
+            createdAt: post?.createdAt,
+            blogName: post?.blogName
+        }
+    })
 
-    res.send(allPosts)
+    res.send(returnAllPosts)
 })
 
-postRouter.get('/:id', findPost, (req: Request, res: Response) => {
+postRouter.get('/:id', findPost, async (req: Request, res: Response) => {
     const result = validationResult(req)
     const errors = ValidateError(result)
     if (errors) {
@@ -28,7 +37,8 @@ postRouter.get('/:id', findPost, (req: Request, res: Response) => {
         }
     }
     const id = req.params.id
-    const post = PostRepository.getPostById(id)
+    const post = await PostRepository.getPostById(id)
+    console.log(post)
     if (!post) {
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
         return;
@@ -88,23 +98,23 @@ postRouter.delete('/:id', authMiddleware, findPost, async (req: Request, res: Re
     return;
 })
 
-// postRouter.put('/:id', authMiddleware, findPost, postValidation(), (req: Request, res: Response) => {
-//     const result = validationResult(req)
-//     const errors = ValidateError(result)
-//     if (errors) {
-//         if (errors.errorsMessages[0].field === 'id') {
-//             res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-//             return;
-//         }
-//         res.status(HTTP_STATUSES.BAD_REQUEST_400).send(errors)
-//         return;
-//     }
-//
-//     const updatedPost = PostRepository.updatePost(req.params.id, req.body)
-//     if (!updatedPost) {
-//         res.sendStatus(400)
-//         return;
-//     }
-//     res.sendStatus(204)
-//     return
-// })
+postRouter.put('/:id', authMiddleware, findPost, postValidation(), async (req: Request, res: Response) => {
+    const result = validationResult(req)
+    const errors = ValidateError(result)
+    if (errors) {
+        if (errors.errorsMessages[0].field === 'id') {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+            return;
+        }
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send(errors)
+        return;
+    }
+
+    const updatedPost = await PostRepository.updatePost(req.params.id, req.body)
+    if (!updatedPost) {
+        res.sendStatus(400)
+        return;
+    }
+    res.sendStatus(204)
+    return
+})
