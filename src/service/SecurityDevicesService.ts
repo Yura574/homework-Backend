3529cbdb5b29fe37ddd7192b9e72ef2ef36fe01c
Yura <yura5742248@gmail.com>
2024-05-18 20:18:30 +0300

    test

--- src/service/SecurityDevicesService.ts
+++ src/service/SecurityDevicesService.ts
@@ -44,7 +44,6 @@ export class SecurityDevicesService {
     static async getDeviceById(deviceId: string): Promise<ObjectResult<any>> {
 
         const device = await SecurityDevicesRepository.getDeviceById(deviceId)
-        console.log(device)
         if (!device) return {status: ResultStatus.NotFound, errorsMessages: 'Device not found', data: null}
         const newDevice = {
             id: device._id.toString(),
