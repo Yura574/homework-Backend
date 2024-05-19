9db6f24be9f6446b63d2d36fe48eecc3a4a2c3e3
Yura <yura5742248@gmail.com>
2024-05-19 18:03:15 +0300

    fix post likes & dislikes

--- src/middleware/auth/getUserId-middleware.ts
+++ src/middleware/auth/getUserId-middleware.ts
@@ -1,9 +1,10 @@
 import {NextFunction, Response} from "express";
 import jwt from "jsonwebtoken";
 import {RequestType} from "../../routes/blog-router";
+import {UserRepository} from "../../repositories/user-repository";
 
 
-export const getUserId = (req: RequestType<any, any, any>, res: Response, next: NextFunction)=> {
+export const getUserId = async (req: RequestType<any, any, any>, res: Response, next: NextFunction)=> {
 
     const auth = req.headers['authorization'];
     if (!auth) return next()
@@ -13,8 +14,12 @@ export const getUserId = (req: RequestType<any, any, any>, res: Response, next: 
     if (type === 'Bearer') {
         try {
             const dataToken: any = jwt.verify(token, process.env.ACCESS_SECRET as string)
+            const userData = await UserRepository.getUserById(dataToken.userId)
+            if (!userData) return res.sendStatus(401)
+
             req.user = {
                 userId: dataToken.userId,
+                login: userData.login
             }
             return next()
 
